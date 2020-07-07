f1f388722b19a011a5dee190a00a346a8f6c40da
Dr-Anonymous <gangrenesoul@gmail.com>
2020-07-08 00:45:54 +0530

    working score and random generator

--- app/src/main/java/com/orthosam/dotcatch/MainActivity.java
+++ app/src/main/java/com/orthosam/dotcatch/MainActivity.java
@@ -7,16 +7,18 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
 
+public class MainActivity extends AppCompatActivity {
     ImageButton btn1, btn2, btn3, btn4;
-    TextView textView;
+    TextView textView, score;
 
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         //set layout and define objects inside it
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.text);
+        score = findViewById(R.id.score);
         btn1 = findViewById(R.id.imageButton1);
         btn2 = findViewById(R.id.imageButton2);
         btn3 = findViewById(R.id.imageButton3);
@@ -25,55 +27,79 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void buttonOnClick(View view) {
-        switch (view.getId()) {
-            case R.id.imageButton1:
-                if (btn1.getDrawable().getCurrent().isVisible()) {
-                    btn1.setVisibility(View.INVISIBLE);
-                    btn2.setVisibility(View.VISIBLE);
-                } else {
-                    textView.setText("Game Over.....");
-                }
-                break;
-            case R.id.imageButton2:
-                if (btn2.getDrawable().isVisible()) {
-                    btn2.setVisibility(View.INVISIBLE);
-                    btn3.setVisibility(View.VISIBLE);
-                    ;
-                } else {
-                    textView.setText("Game Over.....");
-                }
-                break;
-            case R.id.imageButton3:
-                if (btn3.getDrawable().isVisible()) {
-                    btn3.setVisibility(View.INVISIBLE);
-                    btn4.setVisibility(View.VISIBLE);
-                    ;
-                } else {
-                    textView.setText("Game Over.....");
-                }
-                break;
-            case R.id.imageButton4:
-                if (btn4.getDrawable().isVisible()) {
-                    btn4.setVisibility(View.INVISIBLE);
-                    btn1.setVisibility(View.VISIBLE);
-                    ;
-                } else {
-                    textView.setText("Game Over.....");
-                }
-                break;
-
+        try {
+            switch (view.getId()) {
+                case R.id.text:
+                    textView.setText("Catch !!!");
+                    textView.setClickable(false);
+                    set();
+                    break;
+                case R.id.imageButton1:
+                    //getResources().getDrawable(R.drawable.on).getConstantState()
+                    btn1.getDrawable().getConstantState();
+                    btn1.setImageResource(0);
+                    score();
+                    set();
+                    break;
+                case R.id.imageButton2:
+                    btn2.getDrawable().getConstantState();
+                    btn2.setImageResource(0);
+                    score();
+                    set();
+                    break;
+                case R.id.imageButton3:
+                    btn3.getDrawable().getConstantState();
+                    btn3.setImageResource(0);
+                    set();
+                    score();
+                    break;
+                case R.id.imageButton4:
+                    btn4.getDrawable().getConstantState();
+                    btn4.setImageResource(0);
+                    set();
+                    score();
+                    break;
+            }
+        } catch (Exception e) {
+            textView.setText("Game Over\nPlay again?");
+            textView.setClickable(true);
+            btn1.setImageResource(0);
+            btn2.setImageResource(0);
+            btn3.setImageResource(0);
+            btn4.setImageResource(0);
+            score.setText("0");
         }
     }
 
-   /* public void startGame() {
-        try {
-            Thread.sleep( 1000);
-            textView.setText("set");
-            Thread.sleep( 1000);
-            textView.setText("Catch !!!");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
-    }*/
+    public void score() {
+        CharSequence v1 = score.getText();
+        int q = Integer.parseInt(v1.toString());
+        q += 1;
+        score.setText(q + "");
+    }
+
+    public void set() {
+        switch (random()) {
+            case 1:
+                btn1.setImageResource(R.drawable.on);
+                break;
+            case 2:
+                btn2.setImageResource(R.drawable.on);
+                break;
+            case 3:
+                btn3.setImageResource(R.drawable.on);
+                break;
+            case 4:
+                btn4.setImageResource(R.drawable.on);
+                break;
+        }
+    }
+
+
+    public int random() {
+        final int min = 1;
+        final int max = 4;
+        return new Random().nextInt((max - min) + 1) + min;
+    }
 }
