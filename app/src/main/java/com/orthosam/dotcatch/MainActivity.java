e3537edb33e4b1285fbd2e807710b6f15ed75953
Dr-Anonymous <gangrenesoul@gmail.com>
2020-07-09 23:38:59 +0530

    multilevel added

--- app/src/main/java/com/orthosam/dotcatch/MainActivity.java
+++ app/src/main/java/com/orthosam/dotcatch/MainActivity.java
@@ -1,28 +1,49 @@
 package com.orthosam.dotcatch;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    ImageButton btn1, btn2, btn3, btn4;
+    ImageButton b1, b2, b3, b4, b5, b6, b7, b8;
     TextView textView, score;
+    public static String LEVEL;
+    public int level;
 
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        if (intent != null)
+            level = intent.getIntExtra(MainActivity.LEVEL, 1);
+        else
+            level = 1;
+        //check score and level up
+        levelUp(0);
         //set layout and define objects inside it
         setContentView(R.layout.activity_main);
+        //set the title
+        getSupportActionBar().setTitle("Level " + String.valueOf(level));
         textView = findViewById(R.id.text);
         score = findViewById(R.id.score);
-        btn1 = findViewById(R.id.imageButton1);
-        btn2 = findViewById(R.id.imageButton2);
-        btn3 = findViewById(R.id.imageButton3);
-        btn4 = findViewById(R.id.imageButton4);
+        b1 = findViewById(R.id.b1);
+        b2 = findViewById(R.id.b2);
+        b3 = findViewById(R.id.b3);
+        b4 = findViewById(R.id.b4);
+        b5 = findViewById(R.id.b5);
+        b6 = findViewById(R.id.b6);
+        b7 = findViewById(R.id.b7);
+        b8 = findViewById(R.id.b8);
+
         textView.setText("Ready?");
     }
 
@@ -30,76 +51,146 @@ public class MainActivity extends AppCompatActivity {
         try {
             switch (view.getId()) {
                 case R.id.text:
-                    textView.setText("Catch !!!");
+                    textView.setText("Catch the dot!!!");
                     textView.setClickable(false);
                     set();
                     break;
-                case R.id.imageButton1:
+                case R.id.b1:
                     //getResources().getDrawable(R.drawable.on).getConstantState()
-                    btn1.getDrawable().getConstantState();
-                    btn1.setImageResource(0);
+                    b1.getDrawable().getConstantState();
+                    b1.setImageResource(0);
                     score();
                     set();
                     break;
-                case R.id.imageButton2:
-                    btn2.getDrawable().getConstantState();
-                    btn2.setImageResource(0);
+                case R.id.b2:
+                    b2.getDrawable().getConstantState();
+                    b2.setImageResource(0);
                     score();
                     set();
                     break;
-                case R.id.imageButton3:
-                    btn3.getDrawable().getConstantState();
-                    btn3.setImageResource(0);
+                case R.id.b3:
+                    b3.getDrawable().getConstantState();
+                    b3.setImageResource(0);
                     set();
                     score();
                     break;
-                case R.id.imageButton4:
-                    btn4.getDrawable().getConstantState();
-                    btn4.setImageResource(0);
+                case R.id.b4:
+                    b4.getDrawable().getConstantState();
+                    b4.setImageResource(0);
+                    set();
+                    score();
+                    break;
+                case R.id.b5:
+                    b5.getDrawable().getConstantState();
+                    b5.setImageResource(0);
+                    set();
+                    score();
+                    break;
+                case R.id.b6:
+                    b6.getDrawable().getConstantState();
+                    b6.setImageResource(0);
+                    set();
+                    score();
+                    break;
+                case R.id.b7:
+                    b7.getDrawable().getConstantState();
+                    b7.setImageResource(0);
+                    set();
+                    score();
+                    break;
+                case R.id.b8:
+                    b8.getDrawable().getConstantState();
+                    b8.setImageResource(0);
                     set();
                     score();
                     break;
             }
         } catch (Exception e) {
-            textView.setText("Game Over\nPlay again?");
+            textView.setText("Game Over.\nPlay again ?");
             textView.setClickable(true);
-            btn1.setImageResource(0);
-            btn2.setImageResource(0);
-            btn3.setImageResource(0);
-            btn4.setImageResource(0);
+            b1.setImageResource(0);
+            b2.setImageResource(0);
+            b3.setImageResource(0);
+            b4.setImageResource(0);
+            b5.setImageResource(0);
+            b6.setImageResource(0);
+            b7.setImageResource(0);
+            b8.setImageResource(0);
             score.setText("0");
         }
     }
-
 
     public void score() {
         CharSequence v1 = score.getText();
         int q = Integer.parseInt(v1.toString());
         q += 1;
         score.setText(q + "");
+        levelUp(q);
     }
 
     public void set() {
         switch (random()) {
             case 1:
-                btn1.setImageResource(R.drawable.on);
+                b1.setImageResource(R.drawable.on);
                 break;
             case 2:
-                btn2.setImageResource(R.drawable.on);
+                b2.setImageResource(R.drawable.on);
                 break;
             case 3:
-                btn3.setImageResource(R.drawable.on);
+                b3.setImageResource(R.drawable.on);
                 break;
             case 4:
-                btn4.setImageResource(R.drawable.on);
+                b4.setImageResource(R.drawable.on);
                 break;
+            case 5:
+                b5.setImageResource(R.drawable.on);
+                break;
+            case 6:
+                b6.setImageResource(R.drawable.on);
+                break;
+            case 7:
+                b7.setImageResource(R.drawable.on);
+                break;
+            case 8:
+                b8.setImageResource(R.drawable.on);
+                break;
+
         }
     }
 
+    public void levelUp(int i) {
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        int levPoints = 4 * level; //points for each level
+        if (i == 0) {
+            i = sharedPref.getInt(String.valueOf(level), 0);
+            if (i == levPoints) {
+                startActivity(new Intent(this, MainActivity.class).putExtra(LEVEL, level + 1));
+                finish();
+            }
+        } else if (i == levPoints) {
+            //register the final score
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putInt(String.valueOf(level), i);
+            editor.apply();
+            //and level-up
+            Toast.makeText(this, "Level up!!", Toast.LENGTH_LONG).show();
+            startActivity(new Intent(this, MainActivity.class).putExtra(LEVEL, level + 1));
+            finish();
+        } else {
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putInt(String.valueOf(level), i);
+            editor.apply();
+        }
+    }
 
     public int random() {
         final int min = 1;
-        final int max = 4;
+        final int max;
+        if (level == 1)
+            max = 4;
+        else if (level == 2)
+            max = 6;
+        else max = 8;
         return new Random().nextInt((max - min) + 1) + min;
     }
 }
